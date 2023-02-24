8bfe54fd8b1c45c0ee5ac5b56586dc47cdc45299
Joao Oliveira <jaoliveira98@gmail.com>
2023-02-24 14:22:42 +0000

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+import { navLinks } from "../constants/index";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="Logo" className="w-[124px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={open ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setOpen((prev) => !prev)}
+        />
+      </div>
+
+      <div
+        className={`${
+          open ? "flex" : "hidden"
+        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar`}
+      >
+        <ul className="list-none flex justify-end items-center flex-col flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                index === navLinks.length - 1 ? "mb-0" : "mb-4"
+              } text-white`}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
